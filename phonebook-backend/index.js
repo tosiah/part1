886b1019b796d735c26f2bf61ccf06a8c804a6f9
Antonina Hibowska <antonina.hibowska@dotdata.com>
2023-05-09 18:23:45 +0200

    removing app.get('/' from routings

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -37,9 +37,6 @@ let persons = [
     }
 ]
 
-app.get('/', (request, response) => { response.send('phonebook-backend/build/index.html')
-})
-
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
